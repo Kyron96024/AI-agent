564663d36f49e9c66de045caf19f651fee824403
Kyron96024 <gergoondo@gmail.com>
2026-06-23 21:26:13 +0200

    Added loping to create truly agent work

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 from dotenv import load_dotenv
 from google import genai
@@ -21,42 +22,52 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model='gemini-2.5-flash',
-                                               contents=messages,
-                                               config=types.GenerateContentConfig(
-                                                   tools=[available_functions], system_instruction=system_prompt
-                                                   )
-                                               )
 
-    if response.usage_metadata == None:
-        raise RuntimeError("Failed api request")
+    for _ in range(20):
+        response = client.models.generate_content(model='gemini-2.5-flash',
+                                                contents=messages,
+                                                config=types.GenerateContentConfig(
+                                                    tools=[available_functions], system_instruction=system_prompt
+                                                    )
+                                                )
+
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+
+        if response.usage_metadata == None:
+            raise RuntimeError("Failed api request")
+        
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+            print("Response:")
+
+        function_call_results = []
+        if response.function_calls == None:
+            print(response.text)
+            break
+        else:
+            for function_call in response.function_calls:
+                function_call_result = call_function(function_call, verbose=args.verbose)
+
+                if not function_call_result.parts:
+                    raise ValueError("The call_function returned a Content object with an empty 'parts' list!")
+
+                if function_call_result.parts[0].function_response is None:
+                    raise ValueError("The call_function returned a Content object with a 'parts' list where the first part has a 'function_response' of None!")
+                
+                if function_call_result.parts[0].function_response.response is None:
+                    raise ValueError("The call_function returned a Content object with a 'parts' list where the first part has a 'function_response' with a 'response' of None!")
+
+                function_call_results.append(function_call_result.parts[0])
+                if args.verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
     
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        print("Response:")
-
-    function_call_results = []
-    if response.function_calls == None:
-        print(response.text)
+            messages.append(types.Content(role="user", parts=function_call_results))
     else:
-        for function_call in response.function_calls:
-            function_call_result = call_function(function_call, verbose=args.verbose)
-
-            if not function_call_result.parts:
-                raise ValueError("The call_function returned a Content object with an empty 'parts' list!")
-
-            if function_call_result.parts[0].function_response is None:
-                raise ValueError("The call_function returned a Content object with a 'parts' list where the first part has a 'function_response' of None!")
-            
-            if function_call_result.parts[0].function_response.response is None:
-                raise ValueError("The call_function returned a Content object with a 'parts' list where the first part has a 'function_response' with a 'response' of None!")
-
-            function_call_results.append(function_call_result.parts[0])
-            if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-    
-
+        print("Maximum number of iterations reached without a final response.")
+        sys.exit(1)
 if __name__ == "__main__":
     main()
