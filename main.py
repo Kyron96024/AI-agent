b21dcdd69e21fbad087e39bec1d6c4983d799495
Kyron96024 <gergoondo@gmail.com>
2026-06-23 20:56:06 +0200

    Updated the agent so that it can use functions

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+from call_function import available_functions, call_function
+
 
 def main():
     load_dotenv()
@@ -18,7 +21,12 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model='gemini-2.5-flash', contents=messages)
+    response = client.models.generate_content(model='gemini-2.5-flash',
+                                               contents=messages,
+                                               config=types.GenerateContentConfig(
+                                                   tools=[available_functions], system_instruction=system_prompt
+                                                   )
+                                               )
 
     if response.usage_metadata == None:
         raise RuntimeError("Failed api request")
@@ -28,7 +36,27 @@ def main():
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
         print("Response:")
+
+    function_call_results = []
+    if response.function_calls == None:
         print(response.text)
+    else:
+        for function_call in response.function_calls:
+            function_call_result = call_function(function_call, verbose=args.verbose)
+
+            if not function_call_result.parts:
+                raise ValueError("The call_function returned a Content object with an empty 'parts' list!")
+
+            if function_call_result.parts[0].function_response is None:
+                raise ValueError("The call_function returned a Content object with a 'parts' list where the first part has a 'function_response' of None!")
+            
+            if function_call_result.parts[0].function_response.response is None:
+                raise ValueError("The call_function returned a Content object with a 'parts' list where the first part has a 'function_response' with a 'response' of None!")
+
+            function_call_results.append(function_call_result.parts[0])
+            if args.verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+    
 
 if __name__ == "__main__":
     main()
